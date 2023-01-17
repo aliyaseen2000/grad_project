3a1237755a1a79a3d306fe7af6e91c8e860ed72b
aliyaseen2000 <93789106+aliyaseen2000@users.noreply.github.com>
2023-01-17 16:06:42 +0200

    Add files via upload

---
+++ lib/pages/notification.dart
@@ -0,0 +1,30 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+
+class notify {
+  static Future<bool> create_notification(
+      String t, String d, int idd, String s) async {
+    List time = t.split(":");
+    List date = d.split("/");
+    final AwesomeNotifications awesomeNotifications = AwesomeNotifications();
+    return awesomeNotifications.createNotification(
+      content: NotificationContent(
+        id: idd,
+        title: "Hemoglobin",
+        body: "don't forget your test ${Emojis.smile_face_with_open_mouth}",
+        channelKey: s,
+        notificationLayout: NotificationLayout.Default,
+        wakeUpScreen: true,
+      ),
+      schedule: NotificationCalendar(
+        day: int.parse(date[0]),
+        month: int.parse(date[1]),
+        year: int.parse(date[2]),
+        hour: int.parse(time[0]),
+        minute: int.parse(time[1]),
+        second: 0,
+        millisecond: 0,
+        repeats: true,
+      ),
+    );
+  }
+}
