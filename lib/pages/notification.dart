446b5ecd1558b1f2f6a321def394eccd40bae3aa
aliyaseen2000 <93789106+aliyaseen2000@users.noreply.github.com>
2023-01-19 00:34:04 +0200

    notification.dart

--- lib/pages/notification.dart
+++ lib/pages/notification.dart
@@ -4,7 +4,7 @@ class notify {
   static Future<bool> create_notification(
       String t, String d, int idd, String s) async {
     List time = t.split(":");
-    List date = d.split("/");
+    List date = d.split("-");
     final AwesomeNotifications awesomeNotifications = AwesomeNotifications();
     return awesomeNotifications.createNotification(
       content: NotificationContent(
@@ -16,9 +16,9 @@ class notify {
         wakeUpScreen: true,
       ),
       schedule: NotificationCalendar(
-        day: int.parse(date[0]),
+        year: int.parse(date[0]),
         month: int.parse(date[1]),
-        year: int.parse(date[2]),
+        day: int.parse(date[2]),
         hour: int.parse(time[0]),
         minute: int.parse(time[1]),
         second: 0,
